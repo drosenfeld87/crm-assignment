6c050e9f1ddfd4c8139ec529c59473a8af9e1029
debbie <drosenfeld87@gmail.com>
2018-02-12 14:44:35 -0500

    add new contacts to crm

--- contact.rb
+++ contact.rb
@@ -1,3 +1,4 @@
+gem 'activerecord', '=4.2.10'
 require 'active_record'
 require 'mini_record'
 
@@ -5,7 +6,9 @@ ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'crm.sqlit
 
 
 class Contact < ActiveRecord::Base
+
 # attr_accessor :first_name, :last_name, :email, :note, :id
+
   field :first_name, as: :string
   field :last_name,  as: :string
   field :email,      as: :string
